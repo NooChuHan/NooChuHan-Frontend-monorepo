b589a4b53f0b88c5fd265a3c5bf0c8bb16ef3bc0
kimseungdeok <tmdejr1117@gmail.com>
2024-03-03 17:33:38 +0900

    feat: WIP md api 생성 후 적용 테스트중

--- apps/web/src/app/[post]/page.tsx
+++ apps/web/src/app/[post]/page.tsx
@@ -4,25 +4,60 @@ import React = require('react');
 import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
 import { getPostData } from '../../../utils/get-post-data';
-import mdfile from '../../../posts/test.md';
-
-// const { default: markdown } = await import(`src/contents/blog/${path}`); // 이걸로 대체
+// import mdfile from '../../../posts/test.md';
+import axios from 'axios';
+import { AxiosStatic } from 'axios';
 
 export default function Post() {
   const [html, setHtml] = useState<string>('');
+  
   const pathname = usePathname();
 
+  const getMd = async () => {
+    const { data } = await axios.get<string>('/api/post-md', {
+      params: {
+        title: pathname,
+      },
+    });
+    return data;
+  };
+
   const postsDirectory = pathname.concat(process.cwd(), 'posts');
+  // eslint-disable-next-line no-console
   console.log(process.cwd());
   // console.log(pathname);
-  const postData = getPostData(pathname);
+  // const postData = getPostData(pathname);
   useEffect(() => {
-    markdownToHtml({
-      // markdown: JSON.stringify(mdfile, 2, null),
-      markdown: mdfile,
-    }).then(result => {
-      setHtml(result.toString());
-    });
+    getMd()
+      .then(mdfile => {
+        markdownToHtml({
+          // markdown: JSON.stringify(mdfile, 2, null),
+          markdown: mdfile as string,
+        })
+          .then(result => {
+            setHtml(result.toString());
+            console.log(result.toString());
+          })
+          .catch(err => {
+            // eslint-disable-next-line no-console
+            console.error(err);
+          });
+      })
+      .catch(err => {
+        // eslint-disable-next-line no-console
+        console.error(err);
+      });
+    // markdownToHtml({
+    //   // markdown: JSON.stringify(mdfile, 2, null),
+    //   markdown: mdfile as string,
+    // })
+    //   .then(result => {
+    //     setHtml(result.toString());
+    //   })
+    //   .catch(err => {
+    //     // eslint-disable-next-line no-console
+    //     console.error(err);
+    //   });
   }, []);
 
   return <div dangerouslySetInnerHTML={{ __html: html }} />;
--- apps/web/src/app/api/post-md.ts
+++ apps/web/src/app/api/post-md.ts
@@ -1,5 +1,16 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import fs from 'fs';
+import path from 'path';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function postMd(req: NextApiRequest, res: NextApiResponse) {
+  // MD 파일의 경로를 설정합니다. 이 경로는 프로젝트 구조에 따라 다를 수 있습니다.
+  const filePath = path.join(process.cwd(), 'posts', `${req}.md`);
 
+  // 파일의 내용을 동기적으로 읽습니다.
+  const content = fs.readFileSync(filePath, 'utf8');
+
+  console.log(req, filePath, content);
+
+  // 응답으로 파일의 내용을 반환합니다.
+  res.status(200).send(content);
 }
